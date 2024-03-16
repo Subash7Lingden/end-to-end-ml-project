7f8612442abcf8d6c99620ef4b439477387b2bcd
Subash7Lingden <subashsubbalingden@gmail.com>
2024-03-16 13:03:07 +0545

    data ingestion added

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
+STAGE_NAME = "Data Ingestion stage"
 
 
-logger.info("My name is Subash Kumar Lingden")
+try:
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>stage{STAGE_NAME} completed<<<<<\n\nx=======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
